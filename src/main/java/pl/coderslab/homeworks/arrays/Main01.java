9e0b55449731b112bbb76bb19975f060ba6b8552
Maciej Korczala <maciej.coders@gmail.com>
2021-05-18 22:01:38 +0200

    Methods and arrays - exercises and 2nd solutions

--- src/main/java/pl/coderslab/homeworks/arrays/Main01.java
+++ src/main/java/pl/coderslab/homeworks/arrays/Main01.java
@@ -1,14 +1,55 @@
 package pl.coderslab.homeworks.arrays;
 
 
+import java.util.Arrays;
+
 public class Main01 {
 
     public static void main(String[] args) {
 
+// 2 solutions:
 
+        int[] arr = {2, 4, 6, 2, 4, 9, 19, 22, 1, 0, -4, 43, 3, -4, 4, 22, 2, 7, 19};
+//        int[] uniqueArray = returnUnique(arr);
+        int[] uniqueArray = returnUnique2(arr);
+        System.out.println("Tablica pierwotna po wyjściu z metody: " + Arrays.toString(arr));
+        System.out.println("Tablica unikalnych wartości: " + Arrays.toString(uniqueArray));
     }
-//    public static int[] returnUnique(int[] arr){
-//
-//    }
 
+    public static int[] returnUnique(int[] arr) {
+        int[] uniqueValues = new int[1];
+        System.out.println("Przed sortowaniem: " + Arrays.toString(arr));
+//        int[] arrClone = arr.clone();
+        int[] arrClone = Arrays.copyOf(arr, arr.length);
+        Arrays.sort(arrClone);
+        uniqueValues[0] = arrClone[0];
+        System.out.println("Sortowanie we wnętrzu metody: " + Arrays.toString(arrClone));
+        for (int i = 1; i < arrClone.length; i++) {
+            if (arrClone[i] != arrClone[i - 1]) {
+                uniqueValues = Arrays.copyOf(uniqueValues, uniqueValues.length + 1);
+                uniqueValues[uniqueValues.length - 1] = arrClone[i];
+            }
+        }
+        return uniqueValues;
+    }
+
+    // Wersja z zachowaniem kolejności:
+    public static int[] returnUnique2(int[] arr) {
+        int[] uniqueValues = new int[1];
+        uniqueValues[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            int checkUniqueness = 0;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] == arr[j]) {
+                    checkUniqueness = -1;
+                    break;
+                }
+            }
+            if (checkUniqueness == 0) {
+                uniqueValues = Arrays.copyOf(uniqueValues, uniqueValues.length + 1);
+                uniqueValues[uniqueValues.length - 1] = arr[i];
+            }
+        }
+        return uniqueValues;
+    }
 }
--- src/main/java/pl/coderslab/homeworks/arrays/Main03.java
+++ src/main/java/pl/coderslab/homeworks/arrays/Main03.java
@@ -1,6 +1,8 @@
 package pl.coderslab.homeworks.arrays;
 
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
@@ -8,11 +10,23 @@ import java.util.Random;
 public class Main03 {
 
     public static void main(String[] args) {
+// 2 solutions:
+
         int[] arr = new int[10];
         randomArrGen(arr);
-        System.out.println("Generated Array: "+ Arrays.toString(arr));
-        int element = 8;
+        System.out.println("Generated Array: " + Arrays.toString(arr));
+        String genArrayInString = Arrays.toString(arr);
+        System.out.println("Generated Array in String (check): " + genArrayInString);
+        int element = 1;
         System.out.println("Is in the Array the element present: " + contains(arr, element));
+
+        int check = Arrays.binarySearch(arr, element);
+        if (check >= 0) {
+            System.out.println("2nd solution: Is in the Array the element present: true");
+        } else {
+            System.out.println("2nd solution: Is in the Array the element present: false");
+        }
+
     }
 
     public static boolean contains(int[] arr, int element) {
