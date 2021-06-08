48c867d4e222cc9bedb1f3c070f8660bc26e2f15
Maciej Korczala <maciej.coders@gmail.com>
2021-06-08 23:47:35 +0200

    Exercises - classes File, Files

--- src/main/java/pl/coderslab/homeworks/files/Main01.java
+++ src/main/java/pl/coderslab/homeworks/files/Main01.java
@@ -1,10 +1,24 @@
 package pl.coderslab.homeworks.files;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main01 {
 
     public static void main(String[] args) {
 
+
+    }
+
+    public static int count(String fileName){
+        File file = new File(fileName);
+        try(Scanner scan = new Scanner(file)){
+
+        } catch (FileNotFoundException e){
+            System.out.println("Podany plik nie został znaleziony");
+        }
 
     }
 
