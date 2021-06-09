d93c740f7bf9cdb560b7806e8e6a05e6a1327d4c
Maciej Korczala <maciej.coders@gmail.com>
2021-06-09 23:53:05 +0200

    Exercises - classes File, Files, v. 2

--- src/main/java/pl/coderslab/homeworks/files/Main01.java
+++ src/main/java/pl/coderslab/homeworks/files/Main01.java
@@ -3,6 +3,8 @@ package pl.coderslab.homeworks.files;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main01 {
@@ -14,11 +16,26 @@ public class Main01 {
 
     public static int count(String fileName){
         File file = new File(fileName);
+        int numberOfSignsInFile = 0;
+        int counter = 0;
         try(Scanner scan = new Scanner(file)){
-
+            while(scan.hasNextLine()){
+                String line = scan.nextLine();
+                numberOfSignsInFile += line.length();
+                counter++;
+            }
         } catch (FileNotFoundException e){
             System.out.println("Podany plik nie został znaleziony");
         }
+
+        if(counter > 0)
+            return numberOfSignsInFile;
+        return -1;
+    }
+
+//2 Solution:
+    public static int count(String fileName){
+        Path path = Paths.get(fileName);
 
     }
 
