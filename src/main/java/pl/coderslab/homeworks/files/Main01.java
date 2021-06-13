af0201250b07dc205344c83fcb940abb641e1561
Maciej Korczala <maciej.coders@gmail.com>
2021-06-13 23:56:34 +0200

    Exercises - classes File, Files, v. 2.3

--- src/main/java/pl/coderslab/homeworks/files/Main01.java
+++ src/main/java/pl/coderslab/homeworks/files/Main01.java
@@ -11,8 +11,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main01 {
+//2 Solutions
 
     public static void main(String[] args) {
+        String fileDirectory = "src/main/java/pl/coderslab/homeworks/files/task2.txt";
+        int numberOfSignsInFile = count(fileDirectory);
+        if(numberOfSignsInFile >= 0){
+            System.out.println("liczba znaków w pliku to: " + numberOfSignsInFile);
+        } else if (numberOfSignsInFile < 0)
+            System.out.println("plik jest pusty " + numberOfSignsInFile );
 
 
     }
@@ -33,18 +40,18 @@ public class Main01 {
 
         if(counter > 0)
             return numberOfSignsInFile;
-        return -1;
+        return -5;                          //NEW: mimo błędu wykonało się to co poniżej catcha - return jest tu konieczny
     }
 
 //2 Solution:
-    public static int count(String fileName){
-        Path path = Paths.get(fileName);
-        try {
-            List<String> linesInFile = Files.readAllLines(path);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
+//    public static int count2(String fileName){
+//        Path path = Paths.get(fileName);
+//        try {
+//            List<String> linesInFile = Files.readAllLines(path);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 
 }
